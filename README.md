14e638e3d1f175a18f688f7f969904e13bd4196c
Benedikt Arnold <benedikt.arnold@gmx.net>
2012-10-07 01:52:13 +0200

    initial hacked version

---
+++ README.md
@@ -0,0 +1,12 @@
+test
+====
+This is a short description of this test
+
+a new topic
+-----------
+	
+	and some code
+	change done
+	another change
+	
+	
---
+++ index.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+var userargs = process.argv.slice(2);
+var firstFile = userargs[0];
+var fs = require("fs"),
+app = require("http").createServer(handler),
+io = require('socket.io').listen(app),
+md = require("node-markdown").Markdown;
+
+console.log("Watching "+firstFile);
+
+function emitToSocket(name, socket){
+	fs.readFile(name, function(err,data){
+		var markup = md(data.toString());
+		socket.emit('change', { markup: markup });
+	});
+}
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+app.listen(3000);
+
+io.sockets.on('connection', function (socket) {
+	emitToSocket(firstFile, socket);
+  	fs.watch(firstFile, function(e,filename){
+		var name = filename?filename:firstFile;
+		console.log("Something happens on ",e,name);
+		emitToSocket(name, socket);
+	})
+
+});
+
+
+
